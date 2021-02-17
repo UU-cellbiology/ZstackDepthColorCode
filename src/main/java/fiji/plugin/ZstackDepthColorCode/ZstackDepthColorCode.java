3de280bf059f43d470e024b79cf0485fd4b6bb85
Eugene Katrukha <katpyxa@gmail.com>
2021-02-17 17:42:47 +0100

    v.0.0.2

--- src/main/java/fiji/plugin/ZstackDepthColorCode/ZstackDepthColorCode.java
+++ src/main/java/fiji/plugin/ZstackDepthColorCode/ZstackDepthColorCode.java
@@ -1,5 +1,8 @@
 package fiji.plugin.ZstackDepthColorCode;
 
+
+import java.awt.Color;
+
 import ij.CompositeImage;
 import ij.IJ;
 import ij.ImagePlus;
@@ -8,6 +11,7 @@ import ij.Prefs;
 import ij.WindowManager;
 import ij.gui.GenericDialog;
 import ij.gui.ImageWindow;
+import ij.gui.Toolbar;
 import ij.measure.Calibration;
 import ij.plugin.PlugIn;
 import ij.process.ByteProcessor;
@@ -24,6 +28,13 @@ public class ZstackDepthColorCode implements PlugIn {
 	public void run(final String arg) {
 		
 		int i,j,t,n,x,y;
+		boolean bFramesAsSlices=false;
+		int nLutChoice;
+		int nOutput;
+		boolean bInvertLut;
+		int nColorbar;
+		
+		ByteProcessor ipColorbar; 
 		
 		
 		final ImagePlus imp1 = WindowManager.getCurrentImage();
@@ -32,30 +43,51 @@ public class ZstackDepthColorCode implements PlugIn {
 			return;
 		}
 		//stack dimensions
-		final int channels = imp1.getNChannels();
-		final int frames = imp1.getNFrames();
-		final int slices = imp1.getNSlices();
+		int channels = imp1.getNChannels();
+		int frames = imp1.getNFrames();
+		int slices = imp1.getNSlices();
+		
+		int nCurrSlice=imp1.getSlice();
+		int nCurrFrame=imp1.getFrame();
+		
 		String sStackName = imp1.getTitle();
 
-		if (slices < 2) {
-			IJ.error("Depth color code", "Stack required");
-			return;
-		}
 		if (channels > 1) {
-			IJ.error("Depth color code", "Only one channel stack/hyperstack required");
+			IJ.error("Depth colorcode", "Only one channel stack/hyperstack required");
 			return;
 		}
 		
+		if (slices < 2) {
+			if(frames < 2)
+			{
+				IJ.error("Depth colorcode", "Z-stack/hyperstack is required!");
+				return;
+			}
+			else
+			{
+				IJ.log("Seems like selected image stack contains only time frames.");
+				IJ.log("They will be treated as z-slices.");
+				bFramesAsSlices=true;
+				slices=frames;
+				frames=1;
+			}
+			
+		}
+
+		
 		//ask user for the input
 		GenericDialog dgLUT = new GenericDialog("Choose LUT");
-		int nLutChoice;
-		int nOutput;
+
+		
 		String [] luts = IJ.getLuts();
 		String [] sOutputStack = new String [] {
 				"Color (RGB)", "Composite"};
+		String [] sColorBar = new String [] {
+				"No", "Colorbar with all 256 LUT colors","Colorbar slices only"};
 		dgLUT.addChoice("Use LUT for depth coding:",luts,Prefs.get("ZCodeStack.zLutChoice","Fire"));
 		dgLUT.addCheckbox("Invert LUT? ", Prefs.get("ZCodeStack.invertLut", false));
 		dgLUT.addMessage("NB: current Brightness/Contrast settings will be used for min/max of the output!");
+		dgLUT.addChoice("Generate LUT colorbar image?", sColorBar, Prefs.get("ZCodeStack.Colorbar", "Colorbar with all 256 LUT colors"));
 		dgLUT.addChoice("Output stack format:", sOutputStack, Prefs.get("ZCodeStack.Output", "Color (RGB)"));
 		dgLUT.showDialog();
 		if (dgLUT.wasCanceled())
@@ -64,8 +96,10 @@ public class ZstackDepthColorCode implements PlugIn {
 		nLutChoice = dgLUT.getNextChoiceIndex();
 		Prefs.set("ZCodeStack.zLutChoice", luts[nLutChoice]);
 		String sZLUTName = luts[nLutChoice];
-		boolean bInvertLut = dgLUT.getNextBoolean();
+		bInvertLut = dgLUT.getNextBoolean();
 		Prefs.set("ZCodeStack.invertLut", bInvertLut);
+		nColorbar = dgLUT.getNextChoiceIndex();
+		Prefs.set("ZCodeStack.Colorbar", sColorBar[nColorbar]);
 		nOutput = dgLUT.getNextChoiceIndex();
 		Prefs.set("ZCodeStack.Output", sOutputStack[nOutput]);	
 		
@@ -86,15 +120,15 @@ public class ZstackDepthColorCode implements PlugIn {
 			}
 			else
 			{
-				ish.putPixel(i, 0, (int) Math.round((255) * ((double) (slices-i+2)/ (double) slices)));
+				ish.putPixel(i, 0, (int) Math.round((255) * ((double) (slices-i)/ (double) slices)));
 			}
 			
 		}
 		
 		ImagePlus ccc = new ImagePlus("LUTcode",ish);
 		ccc.show();
-		IJ.run(sZLUTName);
-		IJ.run("RGB Color");
+		IJ.run(ccc,sZLUTName,"");
+		IJ.run(ccc,"RGB Color","");
 		ccc.setSlice(1);
 		final int [][] arrayLUT = new int [slices][3];
 		int [] valrgb = new int [3];
@@ -124,7 +158,10 @@ public class ZstackDepthColorCode implements PlugIn {
 		{
 			for (n = 1; n <= slices; n++) 
 			{
-				imp1.setPositionWithoutUpdate(1, n, t);
+				if(!bFramesAsSlices)
+					{imp1.setPositionWithoutUpdate(1, n, t);}
+				else
+					{imp1.setPositionWithoutUpdate(1, t, n);}
 				finCC.setPositionWithoutUpdate(1, n, t);
 				ip1 = imp1.getProcessor();
 				
@@ -154,11 +191,17 @@ public class ZstackDepthColorCode implements PlugIn {
 				IJ.showProgress((double)((t-1)*slices+n)/(double)total_time);
 			}
 		}
+		// restore the initial position
+		imp1.setPositionWithoutUpdate(1,nCurrSlice,nCurrFrame);
 		
+		ImageWindow winD = null;
+		ImagePlus outIP = null;
 		//output as RGB stack
 		if (nOutput==0)
 		{
-			finCC.show();
+			finCC.setCalibration(cal);
+			outIP=finCC;
+
 		}
 		//output as Composite stack
 		else
@@ -181,17 +224,73 @@ public class ZstackDepthColorCode implements PlugIn {
 			ImagePlus imp2 = new ImagePlus(finCC.getTitle(), stack2);
 			imp2.setDimensions(3, slices, frames);
 	 		imp2 = new CompositeImage(imp2, IJ.COMPOSITE);
-			imp2.show();
+	 		imp2.setCalibration(cal);
+	 		
+	 		outIP=imp2;
+	
 		}
+		
+		
+		if(!bFramesAsSlices)
+			outIP.setPosition(1, 1, 1);
+ 		else
+ 			outIP.setPosition(1, nCurrFrame, 1);
+ 		
+		outIP.show();
+		outIP.updateAndDraw();
+		winD =outIP.getWindow();
+
 		
 		IJ.showStatus("Generating colorcoded Z-stack...done.");
 		IJ.showProgress(1.1);
-		final ImagePlus impD = WindowManager.getCurrentImage();
-		final ImageWindow winD = impD.getWindow();
+
+		if(nColorbar>0)
+		{
+			int nFinlines;
+			if(nColorbar==1)
+			{
+				nFinlines=256;
+			}
+			else
+			{
+				nFinlines=slices;
+			}
+			
+			ipColorbar = new ByteProcessor(10,nFinlines);
+			for ( i=0; i<nFinlines; i++)
+			{
+				if(!bInvertLut)
+				{
+					for (j=0;j<10;j++)
+						{ipColorbar.putPixel(j, nFinlines-i-1, (int) Math.round((255) * ((double) (i+1) / (double) nFinlines)));}
+				}
+				else
+				{
+					for (j=0;j<10;j++)
+						{ipColorbar.putPixel(j, nFinlines-i-1, (int) Math.round((255) * ((double) (nFinlines-i)/ (double) nFinlines)));}
+				}
+				
+			}
+			
+			ImagePlus cbar = new ImagePlus("colorbar_"+ sStackName,ipColorbar);
+			cbar.show();
+			IJ.run(cbar,sZLUTName,"");
+			IJ.run(cbar,"RGB Color","");
+			Color saveColor = Toolbar.getBackgroundColor();
+			IJ.setBackgroundColor(0, 0, 0);
+			IJ.run(cbar,"Canvas Size...","width="+12+" height="+Integer.toString(nFinlines+2)+" position=Center");
+			IJ.setBackgroundColor(255, 255, 255);
+			IJ.run(cbar,"Canvas Size...","width="+32+" height="+Integer.toString(nFinlines+22)+" position=Center");
+			IJ.setBackgroundColor(saveColor.getRed(),saveColor.getGreen(),saveColor.getBlue());
+			cbar.show();
+			winD =cbar.getWindow();
+			
+		}
 		
-		impD.setCalibration(cal);
 		System.gc();
 		WindowManager.setCurrentWindow(winD);
+		winD.toFront();
+		
 
 	}
 
